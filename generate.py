ade36d4702eb3d66ba061dee35630ef5ede0ba35
Georgios Atheridis <atheridis@tutamail.com>
2021-12-02 12:35:08 +0200

    ypass-temp

---
+++ generate.py
@@ -0,0 +1,75 @@
+import hashlib
+
+CHARACTER_SUBSETS = {
+    "lowercase": "abcdefghijklmnopqrstuvwxyz",
+    "uppercase": "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    "digits": "0123456789",
+    "symbols": "!\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
+}
+
+
+class Pass():
+    def __init__(self, name, site, master, length, counter, rules, exclude):
+        self.name = name
+        self.site = site
+        self.master = master
+        self.length = length
+        self.counter = counter
+        self.rules = rules
+        self.exclude = exclude
+        self.extrasalt = 0
+
+        pool_of_chars = ""
+        for rule in rules:
+            pool_of_chars += CHARACTER_SUBSETS[rule]
+
+        chars = "".join(c for c in pool_of_chars if c not in exclude)
+
+        self.password = ""
+        self.password = self.generate_password(
+            chars, length - len(rules), "sha256"
+        )
+
+        one_char_per_rule = ""
+        for rule in rules:
+            av_chars = "".join(
+                c for c in CHARACTER_SUBSETS[rule] if c not in exclude
+            )
+            value = self.generate_password(av_chars, 1, "sha256")
+            one_char_per_rule += value
+
+        for char in one_char_per_rule:
+            entropy = self.create_entropy("sha256")
+            choice = entropy % len(self.password)
+            self.password = (
+                self.password[:choice] +
+                char +
+                self.password[choice:]
+            )
+
+    def create_entropy(self, hashf):
+        self.extrasalt += 1
+        self.salt = (
+            hex(self.extrasalt)[2:] +
+            self.site +
+            self.name +
+            hex(self.counter)[2:]
+        )
+
+        entropy = hashlib.pbkdf2_hmac(
+            hashf,
+            self.master.encode("utf-8"),
+            self.salt.encode("utf-8"),
+            10_000_000 // (self.length + len(self.rules))
+        ).hex()
+
+        return int(entropy, 16)
+
+    def generate_password(self, chars, max_length, hashf):
+        generated_password = ""
+        while(len(generated_password) < max_length):
+            entropy = self.create_entropy(hashf)
+            choice = entropy % len(chars)
+            generated_password += chars[choice]
+
+        return generated_password
---
+++ main.py
@@ -0,0 +1,43 @@
+"""
+Asks the user for input to generate a password
+"""
+
+from getpass import getpass
+from argparse import ArgumentParser
+from generate import Pass
+from pyperclip import copy
+
+
+def main(argsv):
+    """Main function"""
+    name = input("name: ")
+    site = input("site: ")
+    master = getpass("pass: ")
+
+    length = argsv.length
+    counter = argsv.counter
+    rules = ["lowercase", "uppercase", "digits", "symbols"]
+    if argsv.exclude_paypal:
+        exclude = "`~-_=+[]\\{|};:'\",<.>/?"
+    else:
+        exclude = argsv.exclude
+
+    password = Pass(name, site, master, length,
+                    counter, rules, exclude).password
+
+    copy(password)
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="")
+    parser.add_argument("-l", "--length", type=int, default=16,
+                        help="Set the length of your password (default: 16)")
+    parser.add_argument("-c", "--counter", type=int, default=0,
+                        help="Set a counter for you generated password (default: 0)")
+    parser.add_argument("-e", "--exclude", type=str, default="",
+                        help="Select characters you wish to exclude")
+    parser.add_argument("--exclude-paypal", dest="exclude_paypal", default=False,
+                        action="store_true", help="Special exclude characters for paypal")
+    args = parser.parse_args()
+
+    main(args)
